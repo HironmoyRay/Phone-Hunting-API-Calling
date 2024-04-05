fa68fe8fa4764d04183de9f01cdd029b3e55265e
hironmoy-ray <hpray35@gmail.com>
2024-04-05 23:20:50 +0600

    Update

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 // console.log("Script File Added")
-const loadPhone = async() => {
-    const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
-    const data = await res.json();
-    console.log(data);
-}
+const loadPhone = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displayPhones(phones);
+};
 
-const anotherLoadPhone = () => {
-    fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
-    .then (res => res.json())
-    .then (data => console.log(data))
-}
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phones.forEach((phone) => {
+    console.log(phone);
+    const phoneCart = document.createElement("div");
+    phoneCart.classList = `card card-compact w-96 bg-base-100 shadow-xl`;
+    phoneCart.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.brand}</h2>
+            <p>${phone.phone_name}</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneCart);
+  });
+};
 
 loadPhone();
-anotherLoadPhone();
